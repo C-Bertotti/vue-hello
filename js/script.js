ef86b6e45d15edabc15cda547f9394fe57fdcd5d
Caterina Bertotti <bertotti4997@gmail.com>
2021-03-25 08:33:28 +0100

    ex

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+            message: 'ciaone',
+        }
+    }
+);
+
+
+
+
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
