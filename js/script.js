c7e386bd1605a1333ee6b6f3e8b4ee608765a881
Caterina Bertotti <bertotti4997@gmail.com>
2021-03-25 09:07:53 +0100

    bonus

--- js/script.js
+++ js/script.js
@@ -4,13 +4,25 @@ var app = new Vue(
     {
         el: '#root',
         data: {
-            message: 'ciaone',
+            message: 'CIAONE',
+            btnMessage: 'Fatti salutare',
+            display: 'd--none'
+        },
+        // Bonus:
+        // Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+        methods: {
+            fattiSalutare: function  () {
+                if ( this.display == 'd--none' ) {
+                    this.display = 'd--block';
+                    this.btnMessage = 'Non voglio più essere salutato';
+
+                } else {
+                    this.display = 'd--none';
+                    this.btnMessage = 'Fatti salutare';
+                }
+            }
         }
     }
 );
 
 
-
-
-// Bonus:
-// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
